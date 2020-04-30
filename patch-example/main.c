f7cee4953b27501ed2c087671dd6da9dd0a3d3ca
Christian Dietrich <stettberger@dokucode.de>
2020-04-30 19:09:15 +0200

    forgot main.c for a long time

---
+++ patch-example/main.c
@@ -0,0 +1,21 @@
+#include "wf-userland.h"
+#include <stdio.h>
+
+int foo() {
+    stderr = stdout;
+}
+
+int main(void) {
+    struct wf_configuration config;
+    config.track_threads = true;
+    wf_init(config);
+    wf_thread_birth("main");
+    int i = 1000;
+    while (1) {
+        test0(i, 5);
+        sleep(2);
+        i += 1000;
+        wf_global_quiescence("main", 1);
+    }
+    return 0;
+}
